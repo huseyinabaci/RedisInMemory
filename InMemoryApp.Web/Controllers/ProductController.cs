3814c6ed0de8d10be476ca5faf5a161f6a40d0fb
Hüseyin ABACI <huseyin.abaci@hotmail.com>
2024-02-24 13:28:45 +0300

    Finished InMemory

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InMemoryApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace InMemoryApp.Web.Controllers
@@ -28,6 +29,30 @@ namespace InMemoryApp.Web.Controllers
 			//	_memoryCache.Set<string>("zaman", DateTime.Now.ToString());
 			//}
 
+			MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+
+            //  options.SlidingExpiration = TimeSpan.FromSeconds(10);
+            options.AbsoluteExpiration = DateTime.Now.AddSeconds(10);
+
+			options.Priority = CacheItemPriority.High;
+
+			options.RegisterPostEvictionCallback((key, value, reason, state) =>
+			{
+				_memoryCache.Set("callback", $"{key} ->{value} => sebep:{reason}");
+			});
+
+			_memoryCache.Set<string>("zaman", DateTime.Now.ToString(), options);
+
+			Product p = new Product()
+			{
+				Id = 1,
+				Name = "Kalem",
+				Price = 200
+			};
+
+			_memoryCache.Set<Product>("product:1", p);
+			_memoryCache.Set<double>("money", 100.99);
+
 			return View();
 		}
 
@@ -45,7 +70,15 @@ namespace InMemoryApp.Web.Controllers
 
 			//ViewBag.zaman = _memoryCache.Get<string>("zaman");
 
-            return View();
+			_memoryCache.TryGetValue("zaman", out string zamancache);
+			_memoryCache.TryGetValue("callback", out string callback);
+			ViewBag.zaman = zamancache;
+			ViewBag.callback = callback;
+
+			ViewBag.product = _memoryCache.Get<Product>("product:1");
+
+
+			return View();
 		}
 	}
 }
