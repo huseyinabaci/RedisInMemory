c18523174ecc081d2f670e3d2cd9417a7e688ee2
Hüseyin ABACI <huseyin.abaci@hotmail.com>
2024-02-24 13:09:09 +0300

    Add project files.

---
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryApp.Web.Controllers
+{
+	public class ProductController : Controller
+	{
+		private readonly IMemoryCache _memoryCache;
+
+		public ProductController(IMemoryCache memoryCache)
+		{
+			_memoryCache = memoryCache;
+		}
+
+		public IActionResult Index()
+		{
+			// 1.Yol
+			//_memoryCache.Set<string>("zaman",DateTime.Now.ToString());
+
+			//if (String.IsNullOrEmpty(_memoryCache.Get<string>("zaman")))
+			//{
+            //   _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            //}
+
+			// 2.Yol
+			//if(!_memoryCache.TryGetValue("zaman",out string zamancache))
+			//{
+			//	_memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+			//}
+
+			return View();
+		}
+
+		public IActionResult Show()
+		{
+			// 1.Yol
+			//ViewBag.zaman = _memoryCache.Get<string>("zaman");
+			//_memoryCache.Remove("zaman");
+
+			// 2.Yol
+			//_memoryCache.GetOrCreate<string>("zaman", entry =>
+			//{
+			//	return DateTime.Now.ToString();
+			//});
+
+			//ViewBag.zaman = _memoryCache.Get<string>("zaman");
+
+            return View();
+		}
+	}
+}
